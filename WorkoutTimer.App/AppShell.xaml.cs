8085e271d5bba1b6dfcb7108b562757db1bd261f
Sannah Penelope Azuh Mutanga <penelopemutanga@outlook.com>
2025-10-03 20:31:56 -0500

    Added a menu item for settings

--- WorkoutTimer.App/AppShell.xaml.cs
+++ WorkoutTimer.App/AppShell.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace WorkoutTimer.App
+﻿using System.Windows.Input;
+
+namespace WorkoutTimer.App
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            NavigateToSettingsCommand = new Command(async() => {
+                await DisplayAlert("Menu Item", "Settings selected", "OK");
+                this.FlyoutIsPresented = this.FlyoutBehavior != FlyoutBehavior.Flyout;
+            });
+            BindingContext = this;
         }
+
+        public ICommand NavigateToSettingsCommand { get; private set; }
     }
 }
